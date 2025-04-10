1af2774f3ebd389bbc7ca36d19e89a4999f4674a
Zahra Alhabsi <alhabsi01994@gmail.com>
2025-04-10 11:56:15 +0400

    Add Function (Overloaded)

--- Program.cs
+++ Program.cs
@@ -1,10 +1,26 @@
-﻿namespace Airline_Reservation_Console_System_Project2
+﻿using System.Threading.Channels;
+
+namespace Airline_Reservation_Console_System_Project2
 {
     internal class Program
     {
-        static void Main(string[] args)
+        //______Add Function (Overloaded)________
+        //________to print int_____
+        public static void CalculateFare(int input)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("the result of this operation is: " + input);
         }
+        //_____________to print double______
+        public static void CalculateFare(double input)
+        {
+            Console.WriteLine("the result of this operation is: " + input);
+        }
+        //__________to print string_____
+        public static void CalculateFare(string input)
+        {
+            Console.WriteLine("the result of this operation is: " + input);
+        }
+
+        
     }
 }
