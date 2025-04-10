e85fedb6017f6f97bfd540b0398ac003d5341b4d
Zahra Alhabsi <alhabsi01994@gmail.com>
2025-04-10 10:59:35 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Airline_Reservation_Console_System_Project2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
